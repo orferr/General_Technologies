1a84b3c491174352630d2ad1512a9029eaebd4af
orferr <154352729+orferr@users.noreply.github.com>
2024-02-18 15:18:24 +0100

    Create recDVvbLMRVzvl6Or.sql

---
+++ recDVvbLMRVzvl6Or.sql
@@ -0,0 +1,9 @@
+SELECT *
+FROM users
+WHERE country = 'France';
+
+INSERT INTO tracks (title, duration, genre)
+VALUES
+    ('Song 1', 240, 'Pop'),
+    ('Song 2', 320, 'Rock'),
+    ('Song 3', 180, 'Electronic');
